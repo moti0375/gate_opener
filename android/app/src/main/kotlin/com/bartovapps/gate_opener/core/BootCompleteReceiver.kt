1607fef9c8f8961c4ec996545e875f564d5a845a
Moti Bartov <moti.bartov@anagog.com>
2021-10-23 08:18:03 +0300

    Add BootCompleteReceiver.kt to start the app after phone boot

---
+++ android/app/src/main/kotlin/com/bartovapps/gate_opener/core/BootCompleteReceiver.kt
@@ -0,0 +1,10 @@
+package com.bartovapps.gate_opener.core
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class BootCompleteReceiver : BroadcastReceiver(){
+    override fun onReceive(p0: Context?, p1: Intent?) {
+    }
+}
